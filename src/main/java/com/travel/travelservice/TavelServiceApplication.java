dcb1f0d6c888135357038debeba8d411abe63410
Soowan Lee <lsy9406@gmail.com>
2024-06-02 17:51:51 +0900

    feat 프로젝트 생성

---
+++ src/main/java/com/travel/travelservice/TavelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TavelServiceApplication.class, args);
+    }
+
+}
