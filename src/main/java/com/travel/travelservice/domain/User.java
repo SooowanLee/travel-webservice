623c1bc6e4d1ed98ba1e4ce6a498cbbdddcd2049
Soowan Lee <lsy9406@gmail.com>
2024-06-05 01:01:38 +0900

    feat  회원 저장을 위한 객체 User 생성

---
+++ src/main/java/com/travel/travelservice/domain/User.java
@@ -0,0 +1,50 @@
+package com.travel.travelservice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String profile_image;
+
+    @Builder
+    public User(String username, String email, String password, String profile_image) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profile_image = profile_image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getProfile_image() {
+        return profile_image;
+    }
+}
