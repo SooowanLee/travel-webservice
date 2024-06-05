1cdc6445d9d1638d923d34134e9b5e08f278214b
Soowan Lee <lsy9406@gmail.com>
2024-06-06 01:39:14 +0900

    fead 회원 저장, 전체 조회, 특정 회원 조회 기능 추가

--- src/main/java/com/travel/travelservice/service/UserService.java
+++ src/main/java/com/travel/travelservice/service/UserService.java
@@ -2,8 +2,13 @@ package com.travel.travelservice.service;
 
 import com.travel.travelservice.domain.User;
 import com.travel.travelservice.dto.UserRequestDTO;
+import com.travel.travelservice.dto.UserResponseDTO;
 import com.travel.travelservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -16,8 +21,21 @@ public class UserService {
 
     public Long save(UserRequestDTO userDTO) {
         User user = userDTO.toEntity(userDTO);
-        return user.getId();
+
+        return userRepository.save(user).getId();
     }
 
+    public UserResponseDTO findById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저입니다."));
 
+        return UserResponseDTO.from(user);
+    }
+
+    public List<UserResponseDTO> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> UserResponseDTO.from(user))
+                .collect(Collectors.toList());
+    }
 }
