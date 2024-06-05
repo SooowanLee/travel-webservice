b1125c389d5b3b80b6104e39b40ddd518077f570
Soowan Lee <lsy9406@gmail.com>
2024-06-06 01:42:48 +0900

    feat 회원 엔티티

--- src/main/java/com/travel/travelservice/domain/User.java
+++ src/main/java/com/travel/travelservice/domain/User.java
@@ -1,9 +1,6 @@
 package com.travel.travelservice.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
